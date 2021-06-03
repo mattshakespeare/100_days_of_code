6fed95694deb16b30dfdc3dd2c6c92da4a4419b6
matthew <mattshakespeare45@gmail.com>
2021-06-03 13:18:21 +0100

    completed coffee machine oop

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while True:
+    user = input(f"Select one of the following options {menu.get_items()}: ")
+    if user == 'resource':
+        coffee_maker.report()
+        money_machine.report()
+    elif user == 'off':
+        break
+    else:
+        drink = menu.find_drink(user)
+        if coffee_maker.is_resource_sufficient(drink):
+            money_machine.make_payment(drink.cost)
+        
+        
+        
+
